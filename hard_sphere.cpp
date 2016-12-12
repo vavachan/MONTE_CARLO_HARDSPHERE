368c96aef340fe99ea7a271edfc287303511dd8c
vavachan <varghese.i.am@gmail.com>
2016-12-12 15:57:19 +0530

    g_r agrees with lit value

--- hard_sphere.cpp
+++ hard_sphere.cpp
@@ -28,6 +28,28 @@ int hard_sphere(atom Atoms[],int nAtoms,int j) {
     }
     return 1;
 }
+int check_overlap(atom Atoms[],int nAtoms)
+{
+
+    Vector dr;
+    double rr,r;
+    for(int i=0; i<nAtoms-1; i++) {
+        for(int j=i+1; j<nAtoms; j++)
+        {
+            dr=v_sub(Atoms[i].pos,Atoms[j].pos);
+            dr=VWrap(dr,box);
+            rr=v_dot(dr,dr);
+            r=sqrt(rr);
+            if(r<R_CUT_HS) {
+                //cout<<i<<"\t"<<j<<"\t"<<rr<<"\n";
+                return 0;
+            }
+
+        }
+
+    }
+    return 1;
+}
 
 void mcmove_hardsphere(atom Atoms[],int nAtoms) {
     double POTn,POTo,P,r;
@@ -52,46 +74,6 @@ void mcmove_hardsphere(atom Atoms[],int nAtoms) {
     }
 
 }
-void vmove(atom Atoms[],int nAtoms,Vector box) {
-    double old_vol,lnV0,lnV;
-    double Lnew,f,r,P;
-    old_vol=8*box.x*box.y*box.z;
-    lnV0=log(old_vol);
-    lnV=lnV0+dlnV*(uni_d(rng)-0.5);
-    vol=exp(lnV);
-    Lnew=pow(vol,0.3333333);
-    f=Lnew/box.x;
-    for (int n=0; n<nAtoms; n++) {
-        Atoms[n].pos.x *= f;
-        Atoms[n].pos.y *= f;
-        Atoms[n].pos.z *= f;
-    }
-    P=exp(-1.0/temp*(Press*(old_vol-vol)-nAtoms*temp*log(vol/old_vol)));
-    r=uni_d(rng);
-    Iter_v++;
-    if(r<P)
-    {
-        Nacc_v++;
-        box.x *= f;
-        box.y *= f;
-        box.z *= f;
-        return ;
-    }
-    else
-    {
-        f=1.0/f;
-        for (int n=0; n<nAtoms; n++) {
-            Atoms[n].pos.x *= f;
-            Atoms[n].pos.y *= f;
-            Atoms[n].pos.z *= f;
-        }
-        vol=old_vol;
-        return ;
-    }
-
-
-
-}
 void back_up(atom Atoms[],atom old_Atoms[],int nAtoms) {
     for(int n=0; n<nAtoms; n++) {
         old_Atoms[n].pos.x=Atoms[n].pos.x;
@@ -114,6 +96,50 @@ void reset(atom Atoms[],int nAtoms)
     }
 
 
+}
+void vmove(atom Atoms[],int nAtoms) {
+    double old_vol,lnV0,lnV;
+    double Lnew,f,r,P;
+    double PE_new;
+    int flag=0;
+    old_vol=vol;
+    lnV0=log(old_vol);
+    lnV=lnV0+dlnV*(uni_d(rng)-0.5);
+    vol=exp(lnV);
+    Lnew=pow(vol,0.3333333)/2.0;
+    f=Lnew/box.x;
+    box.x *= f;
+    box.y *= f;
+    box.z *= f;
+    for (int n=0; n<nAtoms; n++) {
+        Atoms[n].pos.x *= f;
+        Atoms[n].pos.y *= f;
+        Atoms[n].pos.z *= f;
+    }
+    flag=check_overlap(Atoms,nAtoms);
+    P=exp(-1.0/(k_b*temp)*(Press*(vol-old_vol)-(nAtoms+1)*k_b*temp*log(vol/old_vol)));
+    r=uni_d(rng);
+    Iter_v++;
+    if(r<P and flag)
+    {
+        Nacc_v++;
+        return ;
+    }
+    else
+    {
+        f=1.0/f;
+        for (int n=0; n<nAtoms; n++) {
+            Atoms[n].pos.x *= f;
+            Atoms[n].pos.y *= f;
+            Atoms[n].pos.z *= f;
+        }
+
+        box.x *= f;
+        box.y *= f;
+        box.z *= f;
+        vol=old_vol;
+        return ;
+    }
 }
 int move_accept(long nn,long no,long nc) {
     double P=0;
@@ -169,6 +195,7 @@ int main() {
     int nAtoms,N;
     long* HISTOGRAM;
     long n;
+    double g_d;
     atom* Atoms;
     cout<<"Enter the number of atoms:\n";
     cin>>nAtoms;
@@ -186,15 +213,32 @@ int main() {
     cin>>N;
     cout<<"temp\n";
     cin>>temp;
-    box=inipos_bcc(Atoms,nAtoms,box,1.3);
+    cin>>Press;
+    cin>>density;
+    vol=nAtoms/density;
+    box.x=pow(vol,0.333333)/2; 
+    box.y=pow(vol,0.333333)/2; 
+    box.z=pow(vol,0.333333)/2; 
+    inipos(Atoms,nAtoms,box);
     density=nAtoms/(2*box.x*2*box.y*2*box.z);
-    cout<<"density:"<<nAtoms/(2*box.x*2*box.y*2*box.z);
+    vol=2*box.x*2*box.y*2*box.z;
+    cout<<"density:"<<nAtoms/(2*box.x*2*box.y*2*box.z)<<"\n";
     clock_t begin=clock();
-    double EqN=100;
+    double EqN=300000;
     int END=0;
+    int rand=0;
+    cout<<"Press="<<Press<<"\n";
     for(int i=0; i<EqN; i++) {
-        for(int n=0; n<nAtoms; n++)
-            mcmove_hardsphere(Atoms,nAtoms);
+        std::uniform_int_distribution<int> uni(0,nAtoms);
+        rand=uni(rng);
+        if(rand<nAtoms+1) {
+            for(int n=0; n<nAtoms; n++)     //MC_Sweep
+                mcmove_hardsphere(Atoms,nAtoms);
+        }
+        else {
+           // vmove(Atoms,nAtoms);
+            density=nAtoms/vol;
+        }
         if(fmod(i,1000)==0)
         {   cout<<i<<"\n";
         }
@@ -202,31 +246,40 @@ int main() {
     cout<<"eq completed\n";
     Nacc=0;
     Iter=0;
+    Nacc_v=0;
+    Iter_v=0;
     int flag=0;
-    n=largest_cluster(Atoms,nAtoms,l,box); // calculate the largest cluster in the initial config.
+    //  n=largest_cluster(Atoms,nAtoms,l,box); // calculate the largest cluster in the initial config.
     //cout<<n<<"\n";
-    int rand=0;
     for(int i=0; i<N; i++) {
-        for(int n=0; n<nAtoms; n++)     //MC_Sweep
-            mcmove_hardsphere(Atoms,nAtoms);
+        std::uniform_int_distribution<int> uni(0,nAtoms);
+        rand=uni(rng);
+        if(rand<nAtoms+1) {
+            for(int n=0; n<nAtoms; n++)     //MC_Sweep
+                mcmove_hardsphere(Atoms,nAtoms);
+        }
+        else {
+            vmove(Atoms,nAtoms);
+            density=nAtoms/vol;
+            cout<<i<<"\t"<<density<<"\n";
+        }
         if(fmod(i,100)==0)
-        {   cout<<i<<"\n";
+        {  // cout<<i<<"\n";
             if(fmod(i,1000)==0)
             {
-                pair_correlation(Atoms,nAtoms,1,box,temp);
+              g_d=pair_correlation(Atoms,nAtoms,1,box,temp);
             }
             else
-                pair_correlation(Atoms,nAtoms,0,box,temp);
+               g_d=pair_correlation(Atoms,nAtoms,0,box,temp);
+	//	cout<<i<<"\t"<<g_d<<"\n";
+	    cout<<i<<"\t"<<1+2.*M_PI*density*pow(R_CUT_HS,3)*g_d/3.0<<"\t"<<g_d<<"\n";
         }
         if(i>1000)
             flag=1;
     }
-    for(int i=0; i<nAtoms; i++)
-    {
-    ;//    cout<<i<<"\t"<<HISTOGRAM[i]<<"\n";
-    }
+    
     print_pos(Atoms,nAtoms);
-    cout<<"acceptance ratio\t"<<float(Nacc)/float(Iter)<<"\n";
+    cout<<"acceptance ratio\t"<<float(Nacc)/float(Iter)<<"\t"<<float(Nacc_v)/float(Iter_v)<<"\n";
     clock_t end =clock();
     double elapsed_time= double (end-begin)/CLOCKS_PER_SEC;
     cout<<"\n"<<elapsed_time;
