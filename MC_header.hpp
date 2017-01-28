fa1915e1d90f979ef61d4f76ae60fa58dd210a6a
vavachan <varghese.i.am@gmail.com>
2017-01-28 07:36:28 +0530

    header

--- MC_header.hpp
+++ MC_header.hpp
@@ -22,12 +22,22 @@ std::ofstream MSD("position_x.dat");
 std::random_device rd;
 std::mt19937 rng(rd());
 std::uniform_real_distribution<double> uni_d(0.0,1.0);
+///////////////////////////////////////////////////////
 double DELTA_T=.001,r_cut=2.5*sigma;//pow(2.0,1.0/6);
-double R_CUT_HS=sigma;
 double RCUT_sq=r_cut*r_cut;
+///////////////////////////////////////////////////////
+double R_CUT_HS=sigma;
+double R_CUT_HS_sq=R_CUT_HS*R_CUT_HS;
+
+double R_V=1.5;
+double R_V_sq=1.5*1.5;
+
+double gap=(R_V-R_CUT_HS)/2.0;
+double gap_sq=(gap)*(gap);
+
 double PE;
 double vol;
-double dlnV=0.001;
+double dlnV=0.002;
 double vir_pre=0;
 double percor;
 double Press=3.0;
